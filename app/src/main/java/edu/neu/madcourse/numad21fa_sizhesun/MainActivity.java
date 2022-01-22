b4cebafb33f38cc448b8624552086786471e287f
sizhesun <sun.siz@northeastern.edu>
2022-01-22 00:46:42 -0500

    Initial project

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_sizhesun/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21fa_sizhesun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
